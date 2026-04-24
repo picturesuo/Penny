8846184889cc465ad1f305510feaae851fb76158
Ben Suo <suobenjamin@gmail.com>
2026-04-24 01:39:18 -0400

    Add Brain UI: apps/web/lib/viewmodels/brain/index.ts

---
+++ apps/web/lib/viewmodels/brain/index.ts
@@ -0,0 +1,8 @@
+export { createBrainViewModel } from "./create-brain-view-model";
+export type {
+  BrainProjectionClaim,
+  BrainProjectionContext,
+  BrainProjectionView,
+  BrainThoughtViewModel,
+  BrainViewModel,
+} from "./types";
