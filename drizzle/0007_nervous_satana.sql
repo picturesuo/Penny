9e654a1f822efec3aeeff94b8ba3847164e956e9
Ben Suo <suobenjamin@gmail.com>
2026-04-27 17:13:44 -0400

    Add artifact compiler migration

---
+++ drizzle/0007_nervous_satana.sql
@@ -0,0 +1,2 @@
+ALTER TYPE "public"."artifact_kind" ADD VALUE 'idea_map_challenge_brief';--> statement-breakpoint
+ALTER TYPE "public"."move_kind" ADD VALUE 'artifact_created' BEFORE 'source.recorded';
