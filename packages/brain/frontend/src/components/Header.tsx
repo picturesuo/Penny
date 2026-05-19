0e4cdb47080c2d28c53cda30430d034791d70d0b
Ben Suo <suobenjamin@gmail.com>
2026-05-18 21:50:39 -0500

    Rename nav Check to Create

--- packages/brain/frontend/src/components/Header.tsx
+++ packages/brain/frontend/src/components/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
   onLogoSelect?: () => void;
 }
 
-export const navItems = ["Brain", "Check", "Learn"] as const;
+export const navItems = ["Brain", "Create", "Learn"] as const;
 
 export function Header({ sessionLabel, thinkingLabel, activeItem = "Brain", onNavItemSelect, onLogoSelect }: HeaderProps) {
   const editionDate = new Intl.DateTimeFormat(undefined, {
